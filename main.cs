058a82143efbb037a067e916da994983e9113f2a
ambi1107 <158661525+ambi1107@users.noreply.github.com>
2024-02-03 03:00:58 +0000

    added age

--- main.cs
+++ main.cs
@@ -3,6 +3,8 @@ using System;
 class Program {
   public static void Main (string[] args) {
   string firstName = "John";
+  string lastName = "Reyes";
+  int age = 20;
   Console.WriteLine ($"Hello World {firstName} {lastName} Age: {age}");
   Console.WriteLine ("Hello World " + firstName + "" + lastName + " " + age);
   }  
