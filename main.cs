02e3e2d037fd062f9defe5dffbf0d6fe383418e8
jfreyes0810 <31803685-jfreyes0810@users.noreply.replit.com>
2024-02-03 02:46:54 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Program {
+  public static void Main (string[] args) {
+  string firstName = "John";
+  Console.WriteLine ($"Hello World {firstName} {lastName}");
+  Console.WriteLine ("Hello World " + firstName + "" + lastName);
+  }  
+}  
