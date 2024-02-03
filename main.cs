482e1b5d4cc8fd6dbcd55d55dfe2beacd484c78d
ambi1107 <158661525+ambi1107@users.noreply.github.com>
2024-02-03 02:56:43 +0000

    activity

--- main.cs
+++ main.cs
@@ -3,7 +3,7 @@ using System;
 class Program {
   public static void Main (string[] args) {
   string firstName = "John";
-  Console.WriteLine ($"Hello World {firstName} {lastName}");
-  Console.WriteLine ("Hello World " + firstName + "" + lastName);
+  Console.WriteLine ($"Hello World {firstName} {lastName} Age: {age}");
+  Console.WriteLine ("Hello World " + firstName + "" + lastName + " " + age);
   }  
 }  
